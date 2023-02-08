73f969ab3a2e50efbb7a54792b67311d7553672b
Jami Orris <81092462+jamimo76@users.noreply.github.com>
2023-02-08 06:00:32 -0500

    form and filter buttons

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
+const DATA = [
+  { id: 'todo-0', name: "Eat", completed: true },
+  { id: 'todo-1', name: "Sleep", completed: false },
+  { id: 'todo-1', name: "Repeat", completed: false }
+];
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App />
+    <App tasks={DATA}/>
   
 );
